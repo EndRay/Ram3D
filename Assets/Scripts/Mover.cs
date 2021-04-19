10678bc81dea163b8d4257e4794953cebbfcf312
EndRay <a.i.kovrigin@gmail.com>
2021-04-20 00:21:12 +0300

    added Mover

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    public float dragForce;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void FixedUpdate()
+    {
+        float deltaTime = Time.fixedDeltaTime;
+        Vector3 forceDirection = Vector3.zero;
+        if (Input.GetKey("w"))
+        {
+            forceDirection += Vector3.forward;
+        }
+        if (Input.GetKey("s"))
+        {
+            forceDirection += Vector3.back;
+        }
+        if (Input.GetKey("a"))
+        {
+            forceDirection += Vector3.left;
+        }
+        if (Input.GetKey("d"))
+        {
+            forceDirection += Vector3.right;
+        }
+        Vector3.Normalize(forceDirection);
+        Debug.Log(forceDirection);
+        forceDirection = forceDirection.normalized;
+        rb.AddForce(forceDirection * dragForce * deltaTime, ForceMode.Impulse);
+    }
+}
